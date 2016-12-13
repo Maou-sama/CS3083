306922bebcdf12b89379842eaace10313e50b479
Minh Ho <ho.cong.minh1996@gmail.com>
2016-12-12 21:56:19 -0500

    Fourth Commit

--- find_folk.py
+++ find_folk.py
@@ -20,7 +20,7 @@ def toMain():
 @app.route('/main')
 def main():
     cursor = conn.cursor()
-    query = 'SELECT start_time, title FROM an_event WHERE (DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 1 DAY) OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 2 DAY) OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 3 DAY)) ORDER BY start_time'
+    query = 'SELECT start_time, title FROM an_event WHERE start_time < DATE_ADD(CURDATE(), INTERVAL 4 DAY) AND start_time > NOW() ORDER BY start_time'
     cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
@@ -39,9 +39,9 @@ def home():
         #Display upcoming events for current user
         query = 'SELECT an_event.event_id, title, start_time, location_name, zipcode, group_name '\
                 'FROM an_event '\
-                    'JOIN sign_up ON sign_up.event_id = an_event.event_id '\
+                'JOIN sign_up ON sign_up.event_id = an_event.event_id '\
                 'JOIN (organize JOIN a_group ON organize.group_id = a_group.group_id) ON organize.event_id = an_event.event_id '\
-                'WHERE username = %s AND(DATE(start_time) = CURDATE() OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 1 DAY) OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 2 DAY) OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 3 DAY)) '\
+                'WHERE username = %s AND start_time < DATE_ADD(CURDATE(), INTERVAL 4 DAY) AND start_time > NOW() '\
                 'ORDER BY start_time'
         cursor.execute(query, (username))
         data = cursor.fetchall()
@@ -54,6 +54,125 @@ def home():
         cursor.close()
         return render_template('home.html', username=username, events=data, interests=interest_data)
 
+#Display page to allow user to add friend
+@app.route('/addfriend')
+def addfriend():
+    #Redirect back to main page if not logged in
+    if 'username' not in session:
+        return redirect(url_for('main'))
+    
+    else:
+        username = session['username']
+        return render_template('addfriend.html')
+
+#Authentication for adding friend
+@app.route('/addfriendAuth', methods=['GET', 'POST'])
+def addfriendAuth():
+    #Redirect back to main page if not logged in
+    if 'username' not in session:
+        return redirect(url_for('main'))
+    #Check if user is already friend then insert into database and return to home page
+    else:
+        username = session['username']
+        friend_to = request.form['friend_to']
+        cursor = conn.cursor()
+        query = 'SELECT * FROM friend WHERE friend_of = %s AND friend_to = %s'
+        cursor.execute(query, (username, friend_to))
+        data = cursor.fetchone()
+
+        if(data):
+            error = "Already friend with this user"
+            cursor.close()
+            return render_template('addfriend.html', error=error)
+        else:
+            ins = 'INSERT INTO friend VALUES (%s, %s)'
+            cursor.execute(ins, (username, friend_to))
+            conn.commit()
+            cursor.close()
+            return redirect(url_for('home'))
+
+#Display page to rate events
+@app.route('/rate')
+def rate():
+    #Redirect back to main page if not logged in
+    if 'username' not in session:
+        return redirect(url_for('main'))
+    
+    else:
+        username = session['username']
+        cursor = conn.cursor()
+        #Get the user's events that have already taken place
+        query = 'SELECT sign_up.event_id, title FROM sign_up JOIN an_event ON sign_up.event_id = an_event.event_id WHERE username = %s AND end_time < NOW()'
+        cursor.execute(query, (username))
+        data = cursor.fetchall()
+
+        cursor.close()
+        return render_template('rate.html', events = data)
+    
+@app.route('/rateAuth', methods=['GET', 'POST'])
+def rateAuth():
+    #Redirect back to main page if not logged in
+    if 'username' not in session:
+        return redirect(url_for('main'))
+    
+    else:
+        username = session['username']
+        event_id = request.form['event']
+        rating = request.form['rating']
+        cursor = conn.cursor()
+        #Check if event is signed up for or if it has happened
+        check = 'SELECT * FROM sign_up JOIN an_event ON sign_up.event_id = an_event.event_id WHERE sign_up.event_id = %s AND username = %s AND end_time < NOW()'
+        cursor.execute(check, (event_id, username))
+        data = cursor.fetchone()
+        if(data):
+            query = 'UPDATE sign_up SET rating = %s WHERE username = %s AND event_id = %s'
+            cursor.execute(query, (rating, username, event_id))
+            conn.commit()
+            cursor.close()
+            return redirect(url_for('home'))
+        else:
+            error = "You haven't signed up for this event or this event haven't happened yet"
+            cursor.close()
+            return render_template('rate.html', error = error)
+
+#Display the average rating of your groups for the last 3 days
+@app.route('/average')
+def average():
+    #Redirect back to main page if not logged in
+    if 'username' not in session:
+        return redirect(url_for('main'))
+    
+    else:
+        username = session['username']
+        cursor = conn.cursor()
+        query = 'SELECT AVG(rating) AS average_rating, group_name, title '\
+                'FROM belongs_to '\
+                'JOIN a_group ON belongs_to.group_id = a_group.group_id '\
+                'JOIN (organize JOIN (an_event JOIN sign_up ON an_event.event_id = sign_up.event_id) ON organize.event_id = an_event.event_id) ON belongs_to.group_id = organize.group_id '\
+                'WHERE belongs_to.username = %s AND end_time >= DATE_ADD(CURDATE(), INTERVAL -4 DAY) AND end_time <= NOW() '\
+                'GROUP BY title'
+        cursor.execute(query, (username))
+        data = cursor.fetchall()
+        cursor.close()
+        return render_template('average.html', ratings=data)
+
+#Display friend's events
+@app.route('/friend-event')
+def friendEvent():
+    #Redirect back to main page if not logged in
+    if 'username' not in session:
+        return redirect(url_for('main'))
+    
+    else:
+        username = session['username']
+        cursor = conn.cursor()
+        query = 'SELECT title, friend_to FROM sign_up JOIN friend ON sign_up.username = friend.friend_to JOIN an_event ON sign_up.event_id = an_event.event_id '\
+                'WHERE friend_of = %s AND start_time < DATE_ADD(CURDATE(), INTERVAL 4 DAY) AND start_time > NOW()'
+        cursor.execute(query, (username))
+        data = cursor.fetchall()
+        cursor.close()
+        return render_template('friend-event.html', events=data)
+    
 #Display form for authorized user to create events
 @app.route('/create')
 def create():
@@ -73,6 +192,7 @@ def create():
 
 @app.route('/createAuth', methods=['GET', 'POST'])
 def createAuth():
+    #Redirect back to main page if not logged in
     if 'username' not in session:
         return redirect(url_for('main'))
     
@@ -125,7 +245,7 @@ def search():
                 'JOIN (organize JOIN an_event ON organize.event_id = an_event.event_id) ON organize.group_id = about.group_id '\
                 'JOIN interested_in ON about.category = interested_in.category AND about.keyword = interested_in.keyword '\
                 'JOIN a_group ON about.group_id = a_group.group_id '\
-                'WHERE username = %s AND (DATE(start_time) = CURDATE() OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 1 DAY) OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 2 DAY) OR DATE(start_time) = DATE_ADD(CURDATE(), INTERVAL 3 DAY)) '\
+                'WHERE username = %s AND start_time < DATE_ADD(CURDATE(), INTERVAL 4 DAY) AND start_time > NOW() '\
                 'ORDER BY start_time'
         cursor.execute(query, (username))
         data = cursor.fetchall()
@@ -154,13 +274,14 @@ def signUp():
         if(data):
             error = 'Already signed up'
             cursor.close()
-            return render_template('search.html', error = error)
+            return render_template('signup.html', error = error)
         else:
             query = 'INSERT INTO sign_up (event_id, username) VALUES (%s, %s)'
             cursor.execute(query, (event_id, username))
             conn.commit()
             cursor.close()
-            return redirect(url_for('search'))
+            success = 'You have successfully sign up for this event'
+            return render_template('signup.html', success = success)
 
 #Display login page
 @app.route('/login')
@@ -198,10 +319,14 @@ def register():
 def registerAuth():
     username = request.form['username']
     password = request.form['password']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    zipcode = request.form['zipcode']
 
     cursor = conn.cursor()
 
-    query = 'SELECT * FROM users WHERE username = %s'
+    query = 'SELECT * FROM member WHERE username = %s'
     cursor.execute(query, (username))
 
     data = cursor.fetchone()
@@ -211,24 +336,12 @@ def registerAuth():
         cursor.close()
         return render_template('register.html', error = error)
     else:
-        ins = 'INSERT INTO users VALUES(%s, (SELECT MD5(%s)))'
-        cursor.execute(ins, (username, password))
+        ins = 'INSERT INTO member VALUES(%s, (SELECT MD5(%s)), %s, %s, %s, %s)'
+        cursor.execute(ins, (username, password, first_name, last_name, email, zipcode))
 
         conn.commit()
         cursor.close()
-        return render_template('index.html')
-    
-
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-    username = session['username']
-    cursor = conn.cursor();
-    blog = request.form['blog']
-    query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
-    cursor.execute(query, (blog, username))
-    conn.commit()
-    cursor.close()
-    return redirect(url_for('home'))
+        return redirect(url_for('main'))
 
 #Logout Authetication
 @app.route('/logout')
